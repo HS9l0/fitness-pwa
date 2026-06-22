ae0c73cc63a5b3d9e49ad5bb32f411ce1d5658be
Lucy Genova <lucygenova@Lucys-MBP.localdomain>
2026-06-22 13:50:22 +0300

    smooth dropdown animation using css grid trick

--- js/screens/plan.js
+++ js/screens/plan.js
@@ -111,8 +111,10 @@ export function renderExerciseCard(ex, num, isWorkoutMode, lastWeights) {
         <div class="ex-meta">${ex.setsLabel}</div>
         <svg class="ex-chevron" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round" width="15" height="15"><polyline points="9 18 15 12 9 6"/></svg>
       </div>
-      <div class="ex-body">
-        ${bodyHtml}
+      <div class="ex-body-outer">
+        <div class="ex-body">
+          ${bodyHtml}
+        </div>
       </div>
     </div>
   `;
